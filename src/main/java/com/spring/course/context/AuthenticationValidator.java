5956f3f9b36567826c11cfd1a0738bfd9648d014
NenadJovcic <nenad.hif@hotmail.se>
2023-11-02 14:48:29 +0100

    User context class added, now can get the user context from request through token etc

---
+++ src/main/java/com/spring/course/context/AuthenticationValidator.java
@@ -0,0 +1,19 @@
+package com.spring.course.context;
+
+
+import com.spring.course.entity.User;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.context.SecurityContextHolder;
+
+public class AuthenticationValidator {
+
+    public static User getAuthenticatedUser( ) {
+        Authentication authentication = SecurityContextHolder.getContext().getAuthentication();
+        if (authentication != null && authentication.getPrincipal() instanceof User) {
+            return (User) authentication.getPrincipal();
+        }
+        return null;
+    }
+
+
+}
