287edcbeeb06e9cf2d092dc33eb4cda9769b0d0f
NenadJovcic <nenad.hif@hotmail.se>
2023-11-08 13:38:10 +0100

    Unauthorized exception class thrown if user is not authorized to access files/folders

---
+++ src/main/java/com/spring/course/exception/UnauthorizedAccessException.java
@@ -0,0 +1,7 @@
+package com.spring.course.exception;
+
+public class UnauthorizedAccessException extends RuntimeException{
+    public UnauthorizedAccessException(String message) {
+        super(message);
+    }
+}
