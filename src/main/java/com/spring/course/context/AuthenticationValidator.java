aba9698ca0a3e86fa876e109ed66bf6fb69334c4
NenadJovcic <nenad.hif@hotmail.se>
2023-11-22 13:29:26 +0100

    updated AuthenticationValidator context class to throw exception

--- src/main/java/com/spring/course/context/AuthenticationValidator.java
+++ src/main/java/com/spring/course/context/AuthenticationValidator.java
@@ -1,6 +1,7 @@
 package com.spring.course.context;
 
 
+import com.spring.course.exception.UserNotFoundException;
 import com.spring.course.user.User;
 import org.springframework.security.core.Authentication;
 import org.springframework.security.core.context.SecurityContextHolder;
@@ -13,13 +14,15 @@ public class AuthenticationValidator {
     /**
      * Retrieves the authenticated user from the security context.
      *
-     * @return The authenticated User or null if not authenticated.
+     * @return The authenticated User if available.
+     * @throws UserNotFoundException if the user is not authenticated or if the authentication
+     *         principal is not an instance of User.
      */
-    public static User getAuthenticatedUser() {
+    public static User getAuthenticatedUser() throws UserNotFoundException {
         Authentication authentication = SecurityContextHolder.getContext().getAuthentication();
         if (authentication != null && authentication.getPrincipal() instanceof User) {
             return (User) authentication.getPrincipal();
         }
-        return null;
+        throw new UserNotFoundException("User not found, please include valid token in request header");
     }
 }
