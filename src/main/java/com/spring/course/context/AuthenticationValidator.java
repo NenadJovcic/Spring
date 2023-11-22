945fe8637f21ceb307a7216b4df6e003ef0f211b
NenadJovcic <nenad.hif@hotmail.se>
2023-11-22 13:06:49 +0100

    - Added comments above code in the user package following Java standard conventions

--- src/main/java/com/spring/course/context/AuthenticationValidator.java
+++ src/main/java/com/spring/course/context/AuthenticationValidator.java
@@ -5,15 +5,21 @@ import com.spring.course.user.User;
 import org.springframework.security.core.Authentication;
 import org.springframework.security.core.context.SecurityContextHolder;
 
+/**
+ * Utility class for validating and retrieving information about the authenticated user.
+ */
 public class AuthenticationValidator {
 
-    public static User getAuthenticatedUser( ) {
+    /**
+     * Retrieves the authenticated user from the security context.
+     *
+     * @return The authenticated User or null if not authenticated.
+     */
+    public static User getAuthenticatedUser() {
         Authentication authentication = SecurityContextHolder.getContext().getAuthentication();
         if (authentication != null && authentication.getPrincipal() instanceof User) {
             return (User) authentication.getPrincipal();
         }
         return null;
     }
-
-
 }
--- src/main/java/com/spring/course/user/AuthenticationResponse.java
+++ src/main/java/com/spring/course/user/AuthenticationResponse.java
@@ -3,6 +3,9 @@ package com.spring.course.user;
 import com.fasterxml.jackson.annotation.JsonInclude;
 import lombok.*;
 
+/**
+ * Response class for authentication containing a JWT token and additional information.
+ */
 @Data
 @Builder
 @AllArgsConstructor
