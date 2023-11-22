744d1e68e5b2b66f403949a702946492da6a774c
NenadJovcic <nenad.hif@hotmail.se>
2023-11-22 12:14:18 +0100

    - Added comments above code in FolderController and Folder entity following Java standard conventions

--- src/main/java/com/spring/course/folder/Folder.java
+++ src/main/java/com/spring/course/folder/Folder.java
@@ -27,12 +27,19 @@ public class Folder {
 
     @NotNull
     private String name;
+
+    // Creation date defaults to the current date and time
     private LocalDateTime createdDate = LocalDateTime.now();
 
-
+    // One-to-many relationship with FileEntity, mapped by the "folder" property
+    // Cascade type is set to ALL for automatic persistence operations
+    // Orphan removal is enabled, meaning if a file is removed from the list, it will be deleted
+    // FetchType is set to LAZY for lazy loading
     @OneToMany(mappedBy = "folder", cascade = CascadeType.ALL, orphanRemoval = true, fetch = FetchType.LAZY)
     private List<FileEntity> files;
 
+    // Many-to-one relationship with User, mapped by the "user_id" column
+    // The relationship is not serialized to JSON to avoid infinite recursion (JsonIgnore)
     @ManyToOne(fetch = FetchType.LAZY)
     @JoinColumn(name = "user_id")
     @JsonIgnore
--- src/main/java/com/spring/course/folder/FolderController.java
+++ src/main/java/com/spring/course/folder/FolderController.java
@@ -1,24 +1,22 @@
 package com.spring.course.folder;
 
-
-import com.spring.course.exception.ResourceNotFoundException;
-import com.spring.course.exception.UnauthorizedAccessException;
 import com.spring.course.file.FileService;
-
 import jakarta.validation.Valid;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-
 import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.*;
 
 
+/**
+ * Controller handling operations related to folders.
+ * Manages HTTP endpoints for creating, retrieving, and deleting folders.
+ */
 @RestController
 @RequestMapping("/api/folders")
 @Validated
 public class FolderController {
-
     @Autowired
     private FolderService folderService;
 
@@ -27,21 +25,44 @@ public class FolderController {
     @Autowired
     private FolderRepository folderRepository;
 
+    /**
+     * Endpoint for creating a new folder.
+     *
+     * @param folderRequest The request body containing folder details.
+     * @return ResponseEntity with the created folder and HTTP status code 201 (Created).
+     */
     @PostMapping("/create")
-    public ResponseEntity<FolderResponse> createFolder(@RequestBody FolderRequest folderRequest) {
+    public ResponseEntity<FolderResponse> createFolder(@Valid @RequestBody FolderRequest folderRequest) {
         return ResponseEntity.status(HttpStatus.CREATED).body(folderService.createFolder(folderRequest));
     }
 
+    /**
+     * Endpoint for retrieving all folders belonging to the authenticated user.
+     *
+     * @return ResponseEntity with the list of folders and HTTP status code 200 (OK).
+     */
     @GetMapping("/all")
     public ResponseEntity<FolderResponse> getAllFoldersByUser() {
         return ResponseEntity.ok(folderService.getAllFoldersByUser());
     }
 
+    /**
+     * Endpoint for retrieving a folder by its ID.
+     *
+     * @param id The ID of the folder to be retrieved.
+     * @return ResponseEntity with the requested folder and HTTP status code 200 (OK).
+     */
     @GetMapping("/{id}")
     public ResponseEntity<FolderResponse> getFolderById(@PathVariable Long id) {
         return ResponseEntity.ok(folderService.getFolderById(id));
     }
 
+    /**
+     * Endpoint for deleting a folder by its ID.
+     *
+     * @param id The ID of the folder to be deleted.
+     * @return ResponseEntity with a message indicating the deletion success and HTTP status code 200 (OK).
+     */
     @DeleteMapping("/{id}")
     public ResponseEntity<FolderResponse> deleteFolderById(@PathVariable Long id) {
         return ResponseEntity.ok(folderService.deleteFolderById(id));
